3d33857c01f3e7360e5ddc790dcba16d5712aa65
Jan de Mooij <A.J.deMooij@uu.nl>
2015-05-18 13:24:49 +0200

    Finishing touch
    * Residuals are now displayed in output file
    * Missing values in the input file no longer mess up the output file
    * Output is split to seperate folder
    * RuntimeWarning handling

--- pl4fitter.py
+++ pl4fitter.py
@@ -1,7 +1,7 @@
 # using http://people.duke.edu/~ccc14/pcfb/analysis.html
 # where x is the concentration, A is the minimum asymptote, B is the steepness, C is the inflection point and D is the maximum asymptote.
 
-import csv, os, re, math, sys
+import csv, os, re, math, sys, warnings
 try:
 	import matplotlib.pyplot as plt
 	from matplotlib.widgets import Button
@@ -22,28 +22,44 @@ class valuesWriter():
 	writer = ""
 
 	def __init__(self):
-		filename = raw_input("Enter the name of the output file you wish to use. Existing files will be overwritten:\n")
-		self.f = open(filename + ".output", 'w')
+		self.filename = raw_input("Enter the name of the output directory you wish to use. Existing files in the directory will be overwritten:\n")
+		try:
+			os.stat(self.filename)
+		except:
+			os.mkdir(self.filename)
+		self.f = open(self.filename + "/" + self.filename + "_output.csv", 'w')
 		self.writeHeader()
 
 	def writeHeader(self):
 		headerrow = "PPId;"
 		for i in range(7):
 			headerrow += ("X%d;" %(i+1))
-			headerrow += ("Y%d;" %(i+1))
-		headerrow += "A;B;C;D;Keep/Discard\n"
+			headerrow += ("Y%d-original;" %(i+1))
+			headerrow += ("Y%d-fit;" %(i+1))
+		headerrow += "A;B;C;D;GoF;Keep/Discard\n"
 		self.f.write(headerrow)
 
 	def shutdown(self):
 		self.f.close()
 
-	def writeRow(self, pp, list1, list2, vals, useful):
+	def writeRow(self, pp, list1, list2, list3, vals, gof, useful):
 		row = "%s;" %pp
 		for i in range(len(list1)):
 			row += "%f;" %list1[i]
 			row += "%f;" %list2[i]
-		row += "%.2f;%.2f;%.2f;%.2f;%s\n" %(vals['A'], vals['B'], vals['C'], vals['D'],useful)
+			row += "%f;" %list3[i]
+		
+		# Handle empty Y-values
+		missingItems = 7 - len(list1)
+		if missingItems > 0:
+			for i in range(missingItems):
+				row += ";;;"
+
+		row += "%f;%f;%f;%f;%f;%s\n" %(vals['A'], vals['B'], vals['C'], vals['D'],gof,useful)
 		self.f.write(row)
+
+	def getFilename(self):
+		return self.filename
 
 def readfile(name):
 	'''
@@ -83,18 +99,22 @@ def readfile(name):
 		return result
 
 def logistic4(x, A, B, C, D):
-    """4PL lgoistic equation."""
-    result = ((A-D)/(1.0+((x/C)**B))) + D
-    return result
+	"""4PL lgoistic equation."""
+	result = ((A-D)/(1.0+((x/C)**B))) + D
+	return result
 
 def residuals(p, y, x):
-    """Deviations of data from fitted 4PL curve"""
-    A,B,C,D = p
-    err = y-logistic4(x, 0, B, C, D)
-    for index, value in enumerate(err):
-    	if math.isnan(value) or B < 0:
-    		err[index] = 1e8
-    return err
+	"""Deviations of data from fitted 4PL curve"""
+	A,B,C,D = p
+	with warnings.catch_warnings():
+		warnings.filterwarnings('error')
+		try:
+			err = y-logistic4(x, 0, B, C, D)
+		except RuntimeWarning:
+			err = list(p)
+			for i in range(len(p)):
+				err[i] = 1e8
+	return err
 
 def peval(x, p):
     """Evaluated value at x with current parameters."""
@@ -126,7 +146,10 @@ def plot(data, sortKeys, index, figure):
 
 	# Plot results
 	figure.clear()
+
 	theplot = plt.plot(x_fitted_plot, peval(x_fitted_plot, plsq[0]),x,y_meas, 'o')
+
+	
 	plt.xlabel('Time (ms)')
 	plt.ylabel('accurary')
 	plt.title('Least-squares 4PL fit to %s data' %pp)
@@ -134,42 +157,46 @@ def plot(data, sortKeys, index, figure):
 	textY = max(y_meas)*0.4
 	for i, (param, est) in enumerate(zip('ABCD',plsq[0])):
 	     plt.text(textX, textY-i*0.2, '%s = %.2f' % (param, est))
-	plt.savefig(pp+'.png')
-	plt.show()
-
+	plt.savefig(writer.getFilename() + "/" + pp + '.png')
+	plt.draw()
 
 	vals = dict()
 	for i, (param, est) in enumerate(zip('ABCD',plsq[0])):
 	     vals[param] = est
 	
+	# Calculate new Y-values and Goodness of Fit (GoF)
+	list3 = [logistic4(x, vals['A'],vals['B'],vals['C'],vals['D']) for x in list1]
+	gof = sum([(y1 - y2)**2 for (y1, y2) in zip(list2,list3)])
+	
 	# Ask user how to mark these values
-	saveValues(pp, list1, list2, vals)
+	saveValues(pp, list1, list2, list3, vals, gof)
 	
 
 
 def selectFile():
 	return raw_input("Enter the name of the source csv file\n")
 
-def saveValues(pp, list1, list2, vals):
-	quest = "Found values: \nA: %.2f\tB: %.2f\tC: %.2f\tD: %.2f\n" %(vals['A'], vals['B'], vals['C'], vals['D'])
+def saveValues(pp, list1, list2, list3, vals, gof):
+	quest = "For participant %s I found these values: \nA: %.2f\tB: %.2f\tC: %.2f\tD: %.2f\n" %(pp, vals['A'], vals['B'], vals['C'], vals['D'])
+	quest += "The goodness of fit is %.2f\n" %gof
 	quest += 'Do you want to mark these values as useful? Press \'U\'. Otherwise, press \'n\'\nTo exit type "exit"\n'
 	useful = raw_input(quest)
 	u = ""
 	if useful in ["u", "U", "USE", "use", "Use","y","Y","yes","YES"]:
-		print "Items will be used! On to the next graph."
+		print "Items will be used! On to the next graph.\n\n"
 		u = "keep"
 	elif useful in ["n", "N", "No", "no", "NO"]:
 		u = "discard"
-		print "These items will not be used! On to the next graph."
+		print "These items will not be used! On to the next graph.\n\n"
 	elif useful == "exit":
 		#user terminated program
 		writer.shutdown()
 		exit(0)
 	else:
 		print "Your input was not recognised."
-		return saveValues(pp, list1, list2, vals)
+		return saveValues(pp, list1, list2, list3, vals, gof)
 
-	writer.writeRow(pp, list1, list2, vals, u)
+	writer.writeRow(pp, list1, list2, list3, vals, gof, u)
 
 if __name__ == "__main__":
 	if len(sys.argv) > 1:
@@ -185,8 +212,12 @@ if __name__ == "__main__":
 	fig1 = plt.figure()
 	fig1.show()
 	plt.ion()
+
 	for i in range(len(sortKeys)):
 		plot(data, sortKeys, i, fig1)
+
+	print "Done! All the entries in the file have been fitted."
+
 	writer.shutdown()
 
 	
